9275f17d97cb1cb194b2fc1af23bc4e3ce9acd41
5kiing <103718596+5kiing@users.noreply.github.com>
2023-02-06 17:50:54 -0700

    mission 5 commit

---
+++ Mission04_masfrod2/controllers/mission05Controller.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission04_masfrod2.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission04_masfrod2.controllers
+{
+    // controller for program created for HomePage and for GradeCalc form
+    public class mission05Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+         // get method for grade calculator
+        [HttpGet]
+        public IActionResult gradeCalc ()
+        {
+            return View();
+        }
+        // post method for grade calculator
+        [HttpPost]
+        public IActionResult gradeCalc (gradeCalcModel model)
+        {
+            return View();
+        }
+    }
+}
---
+++ Mission04_masfrod2/models/gradeCalcModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission04_masfrod2.models
+{
+    public class gradeCalcModel
+    {
+        // data validation, getters, and setters Range is only from 0 to 100
+        // when the data entered is wrong, the webpage will warn the user 
+        //with a custom message
+
+        [Range(0, 100,
+           ErrorMessage = "Value for {0} must be between {1} and {2}.")]
+        public int Assignments { get; set; }
+        [Range(0, 100,
+            ErrorMessage = "Value for {0} must be between {1} and {2}.")]
+        public int Group { get; set; }
+        [Range(0, 100,
+            ErrorMessage = "Value for {0} must be between {1} and {2}.")]
+        public int Quiz { get; set; }
+        [Range(0, 100,
+            ErrorMessage = "Value for {0} must be between {1} and {2}.")]
+        public int Midterm { get; set; }
+        [Range(0, 100,
+            ErrorMessage = "Value for {0} must be between {1} and {2}.")]
+        public int Final { get; set; }
+        [Range(0, 100,
+            ErrorMessage = "Value for {0} must be between {1} and {2}.")]
+        public int Intex { get; set; }
+    }
+}
